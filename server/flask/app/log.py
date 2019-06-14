f8ef594484038bcf4bcddb0bf12705c557e56c69
Apachee <admin@apachee.ru>
2019-06-14 17:06:45 +0300

    Simple utility to make logs

---
+++ server/flask/app/log.py
@@ -0,0 +1,15 @@
+from app import app
+import time
+
+def write(message):
+    try:
+        log_file = open(app.config["LOG_FILE"], "a")
+    except:
+        raise
+    message = time.strftime(" [%H:%M:%S] ", time.gmtime()) + message
+    print("")
+    print(message)
+    print("")
+    log_file.write(message)
+    log_file.write("\n")
+    log_file.close()
