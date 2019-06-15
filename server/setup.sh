7cb3a981997adcc236588bd81728ab0aed222f8b
Apachee <admin@apachee.ru>
2019-06-16 00:27:53 +0300

    Making new setup script that is better

--- server/setup.sh
+++ server/setup.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/bash
+cd flask
+mkdir logs
+
+# We are going to need this
 sudo apt install python3
+python3 -V
+
+# Setting up virtual environment
+
 sudo apt install python3-venv
 python3 -m venv venv
 source venv/bin/activate
-sudo apt install python3-flask
+sudo apt install python3-pip
+pip3 install Flask
+python3 -m flask --version
 deactivate
-mkdir flask/logs
