eaf7312d5e9dcdfceacad2f7e5676a6ecf09aad6
Apachee <admin@apachee.ru>
2019-06-17 16:34:40 +0300

    Some imporovements

--- server/flask/main.py
+++ server/flask/main.py
@@ -1,2 +1,8 @@
 #!python3.7
 from app import app
+import os
+if __name__ == "__main__":
+    if os.environ.get("FLASK_ENV") == "production":
+        app.run(host="127.0.0.1", port=80) # Temporary
+    else:
+        app.run()
