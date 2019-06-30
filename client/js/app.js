1d143fe999e3f62c7f2cb581a1e439427d668e9b
bumboni <yyakut.ac@gmail.com>
2019-06-30 22:39:02 +1100

    added console.

--- client/js/app.js
+++ client/js/app.js
@@ -22,6 +22,18 @@ var cpuUsed = 75;
 var ramUsed = 5;
 var spaceUsed = 95;
 
+var baseAddress = "https://IP:port/method/";
+//TODO(bumbo): having secret key right here in js is not a good idea!
+var key_secret = "123";
+var xhttp = new XMLHttpRequest();
+
+function httpGet(theUrl) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", theUrl, false); 
+	xmlHttp.send(null);
+	return xmlHttp.responseText;
+}
+
 function serverStart() {
 	//TODO(bumbo): use API to start the server
 }
@@ -32,12 +44,15 @@ function serverStop() {
 
 function statsRefresh() {
 	//TODO(bumbo): use API to get the values
+	//NOTE(bumbo): TEST CODE!!!
 	cpuUsed += 20;
 	ramUsed += 20;
 	spaceUsed -= 20;
 	if (spaceUsed < 0) spaceUsed += 100;
 	if (cpuUsed > 100) cpuUsed = 0;
 	if (ramUsed > 100) ramUsed = 0;
+	
+	var ramUsageResponce = httpGet(baseAddress + "stats.get_ram_usage?secret=" + key_secret + "&format=percent");
 
 	cpuLabel.innerHTML = 'CPU (' + cpuUsed + '%)';
 	ramLabel.innerHTML = 'RAM (' + ramUsed + '%)';
@@ -63,11 +78,4 @@ function autoRefreshEnabled() {
 function autoRefreshRate() {
 	clearInterval(lastAutoRefreshTimer);
 	lastAutoRefreshTimer = setInterval(statsRefresh, autoRefreshRate.value);
-}
-
-function httpGet(theUrl) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", theUrl, false); // false for synchronous request
-	xmlHttp.send(null);
-	return xmlHttp.responseText;
 }
---
+++ client/js/console.js
@@ -0,0 +1,29 @@
+var commandText = document.getElementById("commandText");
+var textField = document.getElementById("commandTextField");
+//TODO(bumbo): redraw canvas on widow resize!
+commandText.width = textField.offsetWidth;
+commandText.style.width = textField.offsetWidth + "px";
+commandText.height = 500;
+commandText.style.height = "500px";
+
+textField.addEventListener('keydown', commandEnter);
+
+function sendCommand(command) {
+	//TODO(bumbo): send the command to the server
+}
+
+function commandEnter(event) {	
+	if (event.key === "Enter") {
+		//TODO(bumbo): add command to the stack
+		sendCommand(textField.value);
+		textField.value = "";
+	}
+}
+
+function update() {
+	//TODO(bumbo): get commands from the sevrer
+	var consoleValue = "" 
+	commandText.innerHTML = consoleValue;
+}
+
+setInterval(update, 2000);
